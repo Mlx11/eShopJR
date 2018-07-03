530aa5a6a0a0dfaf0d146b4cc1800b861c573d73
Michael Linder <michael.linder@students.gymneufeld.ch>
2018-07-03 14:48:04 +0200

    Signed-off-by: Michael Linder <michael.linder@students.gymneufeld.ch>

--- eShopJR/src/eshopjr.py
+++ eShopJR/src/eshopjr.py
@@ -3,8 +3,17 @@
 # and open the template in the editor.
 
 from flask import Flask, session
+from flask.ext.mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'mydb'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql = MySQL(app)
+
+
 
 @app.route("/shop")
 def shop():
@@ -23,12 +32,26 @@ def shop():
     return "Template für Wahrenkorb"
 
 
-def login():
-    pass
-
+@app.route('/loginin/', methods=['GET', 'POST'])
+def logingin():
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor()
+        sql = '''SELECT password FROM mydb.kunde WHERE; 
+                 WHERE username = "''' \
+                 + request.form['username'] + '"'
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        password = result[0]
+        if password == request.form['password']:
+            print("loggin correct")
+        else:
+            print("nope")
+            
+            
 def buy(itemID):
     pass
 
 
 if __name__ == "__main__":
+    buy(1)
     app.run(debug=True)
