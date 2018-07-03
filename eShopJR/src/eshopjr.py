4dbc82f08a2c36ef9210401d07f6f3b284bbeb9e
Mlx11 <40787689+Mlx11@users.noreply.github.com>
2018-07-03 10:14:13 +0200

    Add files via upload

--- eShopJR/src/eshopjr.py
+++ eShopJR/src/eshopjr.py
@@ -2,13 +2,33 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
 
 @app.route("/shop")
 def shop():
-    return "Template"
+    return "Template default"
+
+@app.route("/shop/category/<category>")
+def shop():
+    return "Template für Kategorie z.B. CDs"
+
+@app.route("/shop/item/<id>")
+def shop():
+    return "Template für einzelnen Artikel"
+
+@app.route("/shop/warenkorb/")
+def shop():
+    return "Template für Wahrenkorb"
+
+
+def login():
+    pass
+
+def buy(itemID):
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
